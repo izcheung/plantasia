b1f6f8c5a7915c0b03085770b443b6f1f8dc0667
izcheung <icheung24@my.bcit.ca>
2025-11-21 09:27:28 -0800

    feat: set up auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
 const App = () => {
-  return <div>app</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
